bb2918213ed1878d4dd597d107f292876b7563f0
nawaalqahtani <nawa.b.alasmii@gmail.com>
2025-10-29 13:55:38 +0300

    fixed haversine formula in nearest station view and added limit

--- main_app/views.py
+++ main_app/views.py
@@ -26,14 +26,14 @@ class StationViewSet(viewsets.ModelViewSet):
         except (TypeError, ValueError):
             return Response({"error :"}, status=400)
 
-        limit = int(request.query_params.get("limit", 5))
+        limit = int(request.query_params.get("limit", 1))
         R = 6371.0
 
         def haversine(lat1, lon1, lat2, lon2):
             phi1, phi2 = math.radians(lat1), math.radians(lat2)
             dphi = math.radians(lat2 - lat1)
             dlambda = math.radians(lon2 - lon1)
-            a = math.sin(dphi/2)*2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)*2
+            a = math.sin(dphi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
             return 2*R*math.asin(math.sqrt(a))
 
         stations = []
