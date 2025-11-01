a2d309be432cfeeb0c3273891bfd81631f173c0a
nawaalqahtani <nawa.b.alasmii@gmail.com>
2025-11-02 00:10:30 +0300

    add /auth/me route in main app urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LineViewSet, StationViewSet
+from .views import LineViewSet, StationViewSet, RegisterView, MeView
 
 router = DefaultRouter()
 router.register(r"lines", LineViewSet, basename="line")
 router.register(r"stations", StationViewSet, basename="station")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("auth/register/", RegisterView.as_view(), name="auth-register"),
+    path("auth/me/", MeView.as_view(), name="auth-me"),
+    path("", include(router.urls))
+    
+    ]
