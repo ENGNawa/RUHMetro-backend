31bf3d40a47468523fc213b76f4fd52c7074d3d6
nawaalqahtani <nawa.b.alasmii@gmail.com>
2025-11-02 00:12:04 +0300

    register /auth/me endpoint in metro backend urls

--- metro_backend/urls.py
+++ metro_backend/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainView, TokenRefreshView
-from main_app.views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from main_app.views import RegisterView, MeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("main_app.urls")),
 
     path("api/auth/register/", RegisterView.as_view(), name='auth_register'),
-    path("api/auth/login/", TokenObtainView.as_view(), name='auth_login'),
+    path("api/auth/login/", TokenObtainPairView.as_view(), name='auth_login'),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name='auth_refresh'),
+    path("api/auth/me/", MeView.as_view(), name='auth_me'),
+
 ]
