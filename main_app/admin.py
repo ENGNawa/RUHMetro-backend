a907f4c313d3f5a8c42bb79dadee35eb2540fa02
nawaalqahtani <nawa.b.alasmii@gmail.com>
2025-11-02 23:20:03 +0300

    add place to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Line, Station, Category
+from .models import Line, Station, Category, Place
 
 admin.site.register(Line)
 admin.site.register(Station)
 admin.site.register(Category)
+admin.site.register(Place)
