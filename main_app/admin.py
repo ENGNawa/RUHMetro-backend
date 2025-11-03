6edc00f6aac9c09f1951f1c77e84e2b740495871
nawaalqahtani <nawa.b.alasmii@gmail.com>
2025-11-03 11:53:11 +0300

    add Post admin customization

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Line, Station, Category, Place
+from .models import Line, Station, Category, Place, Post
 
 admin.site.register(Line)
 admin.site.register(Station)
 admin.site.register(Category)
 admin.site.register(Place)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_by", "station", "place", "created_at")
+    search_fields = ("title", "body")
+    list_filter = ("station", "place", "created_by")
