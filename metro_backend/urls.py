c2be808667254412a9274617f6fc6dd5544fa47b
nawaalqahtani <nawa.b.alasmii@gmail.com>
2025-11-03 11:29:02 +0300

    add MEDIIA settings for uploaded images

--- metro_backend/urls.py
+++ metro_backend/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from main_app.views import RegisterView, MeView
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("main_app.urls")),
@@ -13,3 +14,5 @@ urlpatterns = [
     path("api/auth/me/", MeView.as_view(), name='auth_me'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
